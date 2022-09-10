9b7c0998a451fc0dcaade2a43dfd3c255c0bf89b
Dave <DJSaunders1997@gmail.com>
2022-09-10 14:02:04 +0100

    Formatting with black

--- twitter_scraping.py
+++ twitter_scraping.py
@@ -1,33 +1,38 @@
-
 import json
 import time
 import pandas as pd
 import requests
 
+
 class twitter_scraper:
     """A class to allow users to easily interface with the twitter API with minimal knowledge.
-    Users simply have to initialise the class with a valid twitter bearer token then call the 
+    Users simply have to initialise the class with a valid twitter bearer token then call the
     get_users_tweets() method with its parameters to retrieve the requested tweet information.
     """
+
     version = "0.1"
     search_url = "https://api.twitter.com/2/tweets/search/all"
     header = ""
 
-    def __init__(self, bearer_token:str) -> None:
+    def __init__(self, bearer_token: str) -> None:
         """Initalises the class with the value of the users bearer_token.
         Prints version num to inform users which version they're using.
-        TODO: Verify token      
+        TODO: Verify token
 
         Args:
             bearer_token (str): Users token required for verification by the twitter API.
         """
-        self.header = {"Authorization": "Bearer {}".format(bearer_token)} # Add verification?
+        self.header = {
+            "Authorization": "Bearer {}".format(bearer_token)
+        }  # Add verification?
 
         print(f"twitter_scraper v{self.version} initialised.")
 
-    # This is essentially the main code of the project all other functions only support this. 
+    # This is essentially the main code of the project all other functions only support this.
     # Does that mean that this should go first?
-    def get_users_tweets(self, users:list[str], start_date:str, end_date:str) -> pd.DataFrame:
+    def get_users_tweets(
+        self, users: list[str], start_date: str, end_date: str
+    ) -> pd.DataFrame:
         """Function to get all tweet ID's from a given date range for a list of users.
         This is the "main" function users should interact with when using this class.
 
@@ -43,9 +48,11 @@ class twitter_scraper:
             pd.DataFrame: DateFrame containing 3 columns 'User', 'TweetCreated', 'TweetId'.
         """
 
-        #Type check on users to only accept a list of strings, including single element strings.
+        # Type check on users to only accept a list of strings, including single element strings.
         if not isinstance(users, list):
-            raise TypeError(message=f"This function only accepts lists of users. {users} is not a valid list of users.")
+            raise TypeError(
+                message=f"This function only accepts lists of users. {users} is not a valid list of users."
+            )
 
         # TODO: accept other datetime formats but convert to expected format before saving as class/instance variables.
         # TODO: Accept multiple datetime formats such as "2020-01-01" and "2020-01-01-07:00:00"
@@ -56,35 +63,39 @@ class twitter_scraper:
         res = []
 
         for user in users:
-            
-            print('-'*30)
-            print(f"User: {user} Number: {users.index(user)+1} of {len(users)}")
-            print('-'*30)
-            print()
-            res.extend( self._users_tweets(user) )
 
-        #Convert results from list of list to DataFrame
+            print("-" * 30)
+            print(f"User: {user} Number: {users.index(user)+1} of {len(users)}")
+            print("-" * 30)
+            print()
+            res.extend(self._users_tweets(user))
+
+        # Convert results from list of list to DataFrame
         # TODO: return more than just tweet ID, then let users filter and choose what they want afterwards.
-        tweets_df = pd.DataFrame.from_records(res, columns=['User', 'TweetCreated', 'TweetId'])
+        tweets_df = pd.DataFrame.from_records(
+            res, columns=["User", "TweetCreated", "TweetId"]
+        )
 
         return tweets_df
 
-    def _connect_to_endpoint(self, query_params:dict) -> json:
+    def _connect_to_endpoint(self, query_params: dict) -> json:
         """Function sends messages to the twitter API endpoint.
         TODO: Maybe rename function to send request or something as that's what it does.
 
         Args:
-            query_params (dict): Dictionary that contains the query details of what tweets twitter should retrieve for us. 
+            query_params (dict): Dictionary that contains the query details of what tweets twitter should retrieve for us.
 
         Raises:
             Exception: Raises if the response from Twitter is ever not just 200 OK.
-            #TODO: Raise specific error type related to requests or networking 
+            #TODO: Raise specific error type related to requests or networking
 
         Returns:
             response.json() (json): Returns the full message response in JSON format.
         """
 
-        response = requests.request("GET", self.search_url, headers=self.header, params=query_params)
+        response = requests.request(
+            "GET", self.search_url, headers=self.header, params=query_params
+        )
 
         print(response.status_code)
         if response.status_code != 200:
@@ -93,13 +104,12 @@ class twitter_scraper:
         else:
             return response.json()
 
-    
-    def _users_tweets(self, user:str) -> list:
+    def _users_tweets(self, user: str) -> list:
         """Get all results from one user.
         Is a hidden helper function that creates the query to send to twitters API
         Also controls the frequency of requests to stay under twitters limit.
-        
-        To query a single users tweets users should use the get_users_tweets 
+
+        To query a single users tweets users should use the get_users_tweets
         function instead of this but with a list containing a single user.
 
         Args:
@@ -112,30 +122,30 @@ class twitter_scraper:
         all_results = []
 
         result_count = 100  # initialise this as a non-zero number
-                            # will be filled with responses from json later 
-                            # can get a max of 100 records at a time
-                            # so if the results aren't 100 we ran out of things to get
-                            # we can halt execution after every 100 records to stay under the twitter limit. 
+        # will be filled with responses from json later
+        # can get a max of 100 records at a time
+        # so if the results aren't 100 we ran out of things to get
+        # we can halt execution after every 100 records to stay under the twitter limit.
 
         # Define template query dict before loop and condition.
         query_params = {
-                        'query': f'(from:{user} -is:retweet) ',
-                        'tweet.fields': 'author_id,id,created_at',
-                        "start_time":f"{self._start_date}",
-                        "end_time":f"{self._end_date}",
-                        "max_results": "100",
-                }
+            "query": f"(from:{user} -is:retweet) ",
+            "tweet.fields": "author_id,id,created_at",
+            "start_time": f"{self._start_date}",
+            "end_time": f"{self._end_date}",
+            "max_results": "100",
+        }
 
         while result_count == 100:
 
             time.sleep(4)
 
-            if num_runs ==0:
+            if num_runs == 0:
                 # No next_token parameter
                 query_params = query_params
             else:
                 # Add next token to params dict
-                query_params["next_token"] = results_json['meta']['next_token']
+                query_params["next_token"] = results_json["meta"]["next_token"]
 
             json_response = self._connect_to_endpoint(query_params)
 
@@ -143,17 +153,19 @@ class twitter_scraper:
             results_json = json.loads(results)
 
             # append this pagnation to all results
-            #all_results.append(results_json)
-            result_count = results_json['meta']['result_count']
+            # all_results.append(results_json)
+            result_count = results_json["meta"]["result_count"]
             num_runs = num_runs + 1
 
             # Only add results if they're non zero
             if result_count > 0:
-                list_of_tweet_dicts = results_json['data']
+                list_of_tweet_dicts = results_json["data"]
 
-                print(f"Tweet {len(all_results)} from {list_of_tweet_dicts[0]['created_at']}")
+                print(
+                    f"Tweet {len(all_results)} from {list_of_tweet_dicts[0]['created_at']}"
+                )
 
                 for tweet in list_of_tweet_dicts:
-                    all_results.append([user, tweet['created_at'], tweet['id']])
+                    all_results.append([user, tweet["created_at"], tweet["id"]])
 
         return all_results
